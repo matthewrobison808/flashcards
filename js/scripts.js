6d59921e9bb3da242cb34c7d6ae5238027df5c29
matthewrobison818 <matthewrobison818@gmail.com>
2017-02-06 13:25:57 -0800

    Add js scripts for 8 elements

---
+++ js/scripts.js
@@ -0,0 +1,93 @@
+$(document).ready(function(){
+  $("#javascript .card-title").click(function(){
+    $("#javascript .card-text").toggle();
+    $("#javascript .card-title").toggle();
+  });
+  $("#javascript .card-text").click(function(){
+    $("#javascript .card-title").toggle();
+    $("#javascript .card-text").toggle();
+  });
+
+  $("#operators .card-title").click(function(){
+    $("#operators .card-text").toggle();
+    $("#operators. card-title").toggle();
+  })
+  $("#operators .card-text").click(function(){
+    $("#operators .card-title").toggle();
+    $("#operators .card-text").toggle();
+  });
+
+  $("#variables .card-title").click(function(){
+    $("#variables .card-text").toggle();
+    $("#variables .card-title").toggle();
+  })
+  $("#variables .card-text").click(function(){
+    $("#variables .card-title").toggle();
+    $("#variables .card-text").toggle();
+  });
+
+  $("#variableconventions .card-title").click(function(){
+    $("#variableconventions .card-text").toggle();
+    $("#variableconventions .card-title").toggle();
+  });
+  $("#variableconventions .card-text").click(function(){
+    $("#variableconventions .card-title").toggle();
+    $("#variableconventions .card-text").toggle();
+  });
+
+  $("#functions .card-title").click(function(){
+    $("#functions .card-text").toggle();
+    $("#functions .card-title").toggle();
+  });
+  $("#functions .card-text").click(function(){
+    $("#functions .card-title").toggle();
+    $("#functions .card-text").toggle();
+  });
+
+  $("#methods .card-title").click(function(){
+    $("#methods .card-text").toggle();
+    $("#methods .card-title").toggle();
+  });
+  $("#methods .card-text").click(function(){
+    $("#methods .card-title").toggle();
+    $("#methods .card-text").toggle();
+  });
+
+  $("#arguments .card-title").click(function(){
+    $("#arguments .card-text").toggle();
+    $("#arguments .card-title").toggle();
+  });
+  $("#arguments .card-text").click(function(){
+    $("#arguments .card-title").toggle();
+    $("#arguments .card-text").toggle();
+  });
+
+  $("#parameters .card-title").click(function(){
+    $("#parameters .card-text").toggle();
+    $("#parameters .card-title").toggle();
+  });
+  $("#parameters .card-text").click(function(){
+    $("#parameters .card-title").toggle();
+    $("#parameters .card-text").toggle();
+  });
+
+  $("#return .card-title").click(function(){
+    $("#return .card-text").toggle();
+    $("#return .card-title").toggle();
+  });
+  $("#return .card-text").click(function(){
+    $("#return .card-title").toggle();
+    $("#return .card-text").toggle();
+  });
+
+  $("#chainingmethods .card-title").click(function(){
+    $("#chainingmethods .card-text").toggle();
+    $("#chainingmethods .card-title").toggle();
+  });
+  $("#chainingmethods .card-text").click(function(){
+    $("#chainingmethods .card-title").toggle();
+    $("#chainingmethods .card-text").toggle();
+  });
+
+  
+});
